c911784e47503cbd70acd9140d19b25f14a35b2b
jerryrox <jerryrox@live.co.kr>
2018-01-29 16:48:23 +1300

    Preparing for new stuff

---
+++ Framework/MVC/UIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Renko.MVC
+{
+	public class UIController : MonoBehaviour {
+
+
+
+
+		void Update() {
+
+		}
+	}
+}
